ee0eb54badd3dd6baf3fcb9f4c0745862a3f7a3a
b-knight <knight.benjamin@gmail.com>
2015-09-17 15:43:21 -0700

    Updated repo with .md file

---
+++ Run_knitr2.R
@@ -0,0 +1,6 @@
+rm(list=ls())
+setwd(paste("/Users/benjaminknight/Documents",
+            "/Personal Training/Trade Project/GitVersion", sep=""))
+install.packages("knitr")
+library(knitr)
+knit2html("HO.Rmd") 
