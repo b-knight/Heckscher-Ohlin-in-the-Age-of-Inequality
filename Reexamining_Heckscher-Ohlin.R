db5b8bfda72dcbbb6d6b48d8d6a04e6a21416820
b-knight <knight.benjamin@gmail.com>
2015-09-18 23:53:45 -0700

    Added code for tidying WIID data set

--- Reexamining_Heckscher-Ohlin.R
+++ Reexamining_Heckscher-Ohlin.R
@@ -1,8 +1,8 @@
 ################################################################################
 ## File name: 	Reexamining_Heckscher-Ohlin.R
-## Date: 		    August 29, 2015
-## Author: 		  Benjamin S. Knight (knight.benjamin@gmail.com)
-## Purpose: 	  Downloads and creates data tables of economic inequality from
+## Date:        August 29, 2015
+## Author:      Benjamin S. Knight (knight.benjamin@gmail.com)
+## Purpose: 	Downloads and creates data tables of economic inequality from
 ##              three major datasets: The Standardized World Income Inequality 
 ##              Database (SWIID) by Frederick Solt, the Estimated Household 
 ##              Income Inequality Data Set by the University of Texas Inequality 
@@ -18,10 +18,8 @@ rm(list=ls())
 # install.packages("gdata")
 # install.packages("data.table")
 devtools::install_github("hadley/haven")
-require(haven)
-# NOTE: You may need to update Java from 
-# https://support.apple.com/kb/DL1572?locale=en_US to run the xlsx package 
-require(xlsx)
+require(haven)     # NOTE: You may need to update Java from 
+require(xlsx)      # https://support.apple.com/kb/DL1572?locale=en_US to run the xlsx package 
 library(gdata)
 library(readstata13)
 library(data.table)
@@ -35,19 +33,16 @@ if (!file.exists("WIID3b_1.xls")) {
                      "/en_GB/wiid/_files/92393927664936620",
                      "/default/WIID3b.xls", sep="")
   download.file(fileUrl_1, destfile = "./WIID3b_1.xls")
-  rm(fileUrl_1)
-}
+  rm(fileUrl_1)}
 if (!file.exists("EHII-UPDATED-10-30-2013.xlsx")) {
   fileUrl_2 <- "http://utip.gov.utexas.edu/data/EHII-UPDATED-10-30-2013.xlsx"
   download.file(fileUrl_2, destfile = "./EHII-UPDATED-10-30-2013.xlsx", 
                 method = "curl")
-  rm(fileUrl_2)
-}
+  rm(fileUrl_2)}
 if (!file.exists("SWIIDv5_0.zip")) {
   fileUrl_3 <- "https://dataverse.harvard.edu/api/access/datafile/2503756"
   download.file(fileUrl_3, destfile = "./SWIIDv5_0.zip", method = "curl") # 11.1mb
-  rm(fileUrl_3)
-}
+  rm(fileUrl_3)}
 # Read the downloaded data into R
 WIID <- data.table(read.xls("WIID3b_1.xls", sheet = "Sheet1")) # 4mb
 EHII <- data.table(read.xlsx2("EHII-UPDATED-10-30-2013.xlsx", 
@@ -55,6 +50,32 @@ EHII <- data.table(read.xlsx2("EHII-UPDATED-10-30-2013.xlsx",
 unzip("SWIIDv5_0.zip", unzip = "internal")
 setwd("./SWIIDv5_0")
 SWIID <- data.table(read_dta("SWIIDv5_0.dta"))
+
+# Clean the WIID data
+library(dplyr)
+WIID <- select(WIID, Countrycode2, Countrycode3, Country, Year, Gini, Quality)
+WIID$Quality <- as.character(WIID$Quality)
+WIID <- filter(WIID,  Year >= 1963 & Year <= 2008)
+dropped_countries <- c("Ussr", "Puerto Rico", "Taiwan", "West Bank And Gaza", "East Timor", "Yugoslavia")
+WIID <- filter(WIID,  !(Country %in% dropped_countries))
+WIID$Quality[WIID$Quality == "High"] <- 3              # assign numeric code to the quality estimate
+WIID$Quality[WIID$Quality == "Average"] <- 2
+WIID$Quality[WIID$Quality == "Low"] <- 1
+WIID$Quality[WIID$Quality == "Not known"] <- 0
+WIID$Quality <- as.numeric(WIID$Quality)                                                 
+WIID$ID <- as.character(paste(WIID$Country, WIID$Year, sep = ""))                    # create an ID variable
+max <- aggregate(x = WIID$Quality, by = list(WIID$Country, WIID$Year), FUN = "max")  # create a max value
+max$ID <- as.character(paste(max$Group.1, max$Group.2, sep = ""))                    # create an ID variable 
+combined <- merge(WIID, max, by = "ID")                                              # merge data sets
+combined <- select(combined, Countrycode2, Countrycode3, Country, Year, Gini, Quality, x)
+combined <- combined[combined$Quality >= combined$x]
+library(doBy)
+combined <- summaryBy(Gini ~ Country + Year, data = combined)
+library(reshape2)
+WIID <- dcast(combined, Country ~ Year, value.var = "Gini.mean")
+rm(max, combined, dropped_countries)
+
+
 
 
 
